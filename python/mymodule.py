ca27bad39bbacb9cdcfd486b66e504afd03d1bfb
medori9999 <ohooong@gmail.com>
2025-09-24 14:52:07 +0900

    update new

---
+++ python/mymodule.py
@@ -0,0 +1,20 @@
+def my_add(a,b):
+    return a + b
+
+def my_sub(a,b):
+    return a - b    
+
+class Character:
+    def __init__(self, name):
+        self.name = name
+    def speak(self):
+        print(f"저는 {self.name}입니다.")
+
+print(f"mymodule안에서의__name__:{__name__}")
+
+if __name__ == "__main__":
+    print(my_add(1,2))
+    print(my_sub(3,4))
+    wizard = Character("법사")
+    wizard.speak()
+
